33d5939d34919ebdbcff7198fa0745e02a77c2e7
Pradip-Garai <pradipgarai173@gmail.com>
2025-09-25 18:26:21 +0530

    updated

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -13,7 +13,7 @@ const ResourceAddRouter = require('../routes/resourseRoute');
 const app = express();
 
 app.use(cors({
-  origin: process.env.CLIENT_URL || 'http://localhost:5173',
+  origin: 'https://codex-one-theta.vercel.app/',
   credentials: true
 }));
 
--- frontend/src/utils/axiosClient.js
+++ frontend/src/utils/axiosClient.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosClient = axios.create({
-  baseURL: 'http://localhost:5000',
+  baseURL: 'https://codex3-puce.vercel.app/',
   withCredentials: true,  // it means we are allowing cookies to be sent with requests
   headers: {
     'Content-Type': 'application/json'
